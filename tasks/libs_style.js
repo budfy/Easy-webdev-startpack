571b14ad58fc81f5ec7ff4a058ff242309ecdeec
budfy <serdyuk.ro@gmail.com>
2022-11-08 09:57:14 +0200

    Fixes SASS for Nodejs@19

--- tasks/libs_style.js
+++ tasks/libs_style.js
@@ -1,25 +1,25 @@
 const plugins = [];
 
 const {
-	src,
-	dest
+  src,
+  dest
 } = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const map = require('gulp-sourcemaps');
 const chalk = require('chalk');
 
 module.exports = function libs_style(done) {
-	if (plugins.length > 0) {
-		return src(plugins)
-			.pipe(map.init())
-			.pipe(sass({
-				outputStyle: 'compressed'
-			}).on('error', sass.logError))
-			.pipe(concat('libs.min.css'))
-			.pipe(map.write('../sourcemaps/'))
-			.pipe(dest('build/css/'))
-	} else {
-		return done(console.log(chalk.redBright('No added CSS/SCSS plugins')));
-	}
+  if (plugins.length > 0) {
+    return src(plugins)
+      .pipe(map.init())
+      .pipe(sass({
+        outputStyle: 'compressed'
+      }).on('error', sass.logError))
+      .pipe(concat('libs.min.css'))
+      .pipe(map.write('../sourcemaps/'))
+      .pipe(dest('build/css/'))
+  } else {
+    return done(console.log(chalk.redBright('No added CSS/SCSS plugins')));
+  }
 }
